9d484a09f95d189b441d98a8c454f0a0bd18c8b3
SimpleLogix <harkouswalid@gmail.com>
2023-07-18 14:16:45 -0400

    project structure init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="root center">
+      <button>Download</button>
+    </div>
+  );
+}
+
+export default App;
